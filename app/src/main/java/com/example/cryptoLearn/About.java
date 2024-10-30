f62adc32d73fb2d818e5fd8e0ceaa1f778488020
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-10-30 11:52:55 +0800

    tangina nyo

--- app/src/main/java/com/example/cryptoLearn/About.java
+++ app/src/main/java/com/example/cryptoLearn/About.java
@@ -1,8 +1,11 @@
 package com.example.cryptoLearn;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView aboutText = findViewById(R.id.aboutText);
+        aboutText.setText(Html.fromHtml(getString(R.string.about_text), Html.FROM_HTML_MODE_COMPACT));
+
+        ImageView backArrow = findViewById(R.id.back_arrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
--- app/src/main/java/com/example/cryptoLearn/Setup.java
+++ app/src/main/java/com/example/cryptoLearn/Setup.java
@@ -3,6 +3,8 @@ package com.example.cryptoLearn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Setup extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Setup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        ImageView backArrow = findViewById(R.id.back_arrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
