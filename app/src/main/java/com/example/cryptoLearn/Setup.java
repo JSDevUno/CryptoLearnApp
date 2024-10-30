8fda31cfe061ce8f7fae00b3659ddc2ff6dc8d17
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-10-30 12:06:25 +0800

    tangina nyo

--- app/src/main/java/com/example/cryptoLearn/Setup.java
+++ app/src/main/java/com/example/cryptoLearn/Setup.java
@@ -6,12 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Setup extends AppCompatActivity {
 
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        textInputLayoutPassword = findViewById(R.id.textInputLayoutPassword);
+        textInputLayoutConfirmPassword = findViewById(R.id.textInputLayoutConfirmPassword);
+
+        textInputLayoutPassword.setHintEnabled(false);
+        textInputLayoutConfirmPassword.setHintEnabled(false);
+
+        textInputLayoutPassword.setEndIconMode(TextInputLayout.END_ICON_PASSWORD_TOGGLE);
+        textInputLayoutPassword.setEndIconActivated(true);
+
+        textInputLayoutConfirmPassword.setEndIconMode(TextInputLayout.END_ICON_PASSWORD_TOGGLE);
+        textInputLayoutConfirmPassword.setEndIconActivated(true);
+
         ImageView backArrow = findViewById(R.id.back_arrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
