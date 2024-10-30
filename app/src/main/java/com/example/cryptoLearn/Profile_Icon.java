8a786a42db61fbf97cde6814eb427e3268c46548
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-10-30 20:41:38 +0800

    tangina nyo

---
+++ app/src/main/java/com/example/cryptoLearn/Profile_Icon.java
@@ -0,0 +1,14 @@
+package com.example.cryptoLearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_Icon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_icon);
+    }
+}
